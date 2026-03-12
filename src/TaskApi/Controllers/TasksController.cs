f6b498e1e591fada7c3a991e8937c45c49dbab9d
Lopez Garcia Mario <mariolopez03@uv.mx>
2026-03-12 11:45:22 -0600

    Se corrige el controller con bugs

--- src/TaskApi/Controllers/TasksController.cs
+++ src/TaskApi/Controllers/TasksController.cs
@@ -23,8 +23,7 @@ public class TasksController : ControllerBase
     public IActionResult GetById(int id)
     {
         var task = _repo.GetById(id);
-        // return task is null ? NotFound() : Ok(task);
-        return BadRequest();
+        return task is null ? NotFound() : Ok(task);
     }
 
     [HttpPost]
